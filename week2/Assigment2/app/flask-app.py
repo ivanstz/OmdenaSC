5219cc856e7ea1eefccc038d6e4393fcc2636455
ivanstz <ivan.tzompov@gmail.com>
2021-12-21 21:27:26 +0200

    Add files via upload

---
+++ week2/Assigment2/app/flask-app.py
@@ -0,0 +1,24 @@
+from simpleinterest import simple_interest
+from flask import Flask, jsonify, request
+
+#instantiate flask objects
+app = Flask('__name__')
+
+@app.route('/', methods=['GET','POST'])
+def get_input():
+
+    """
+    A function to get request using flask, evalete and return result
+    """
+    packet= request.get_json(force=True)
+    principal =packet["principal"]
+    rate = packet["rate"]
+    period = packet["period"]
+
+    interest = simple_interest(principal, rate, period)
+
+    return jsonify(packet,interest)
+
+# driver function
+if __name__ == '_main_':
+    app.run()
---
+++ week2/Assigment2/app/simpleinterest.py
@@ -0,0 +1,7 @@
+def simple_interest(principal, rate, period):
+    """
+    A function to evaluete simple interest
+    """
+    interest = (principal * rate * period)/ 100
+
+    return interest
